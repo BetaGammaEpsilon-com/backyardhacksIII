8afe34389520024ec38dfdba5114ef6a4a4c9a14
JosephBerman <berman.jos@northeastern.edu>
2022-07-30 02:41:28 -0400

    grabs park, gives it an ID, parses name and url

--- backend/api/natpark/NPSGrabber.py
+++ backend/api/natpark/NPSGrabber.py
@@ -1,15 +1,31 @@
 import requests
+import json
+from natpark import NatPark
+
+PARKNUMBER = 467
 
 tokenfile = open("token.txt", 'r')
-
 token = tokenfile.readline().rstrip()
 
-def getAPICall(call: str):
-    endpoint = 'https://developer.nps.gov/api/v1/' + call +'?limit=467&api_key=' + token
+def getAPICall(call: str, limit: int):
+
+    endpoint = 'https://developer.nps.gov/api/v1/' + call +'?limit=' + str(limit) +'&api_key=' + token
     return requests.get(endpoint)
 
 
 def getParks():
-    print(getAPICall('parks/').json())
+    return getAPICall('parks/', PARKNUMBER).json()
 
-getParks()
+
+parks = getParks()
+parkList = []
+i = 0
+for park in parks['data']:
+    parkList.append(
+        NatPark(i,park['fullName'], park['url'])
+    )
+    i = i+1
+
+for i in range(len(parkList)):
+    parkList[i].printPark()
+
--- backend/api/natpark/natpark.py
+++ backend/api/natpark/natpark.py
@@ -3,11 +3,14 @@ import requests
 
 class NatPark:
 
-    def __init__(self):
-        self.id = -1
-        self.name = ""
-        self.url = ""
+    def __init__(self, id: int, name: str, url: str):
+        self.id = id
+        self.name = name
+        self.url = url
         self.reviewCount = -1
         self.stars = -1
 
-
+    def printPark(self):
+        print("ID: " + str(self.id) + "\nName: "
+              + self.name, "\nURL: "
+              + self.url + "\n")
