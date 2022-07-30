cf5701bf2d1cd627ca3fb0a58c7f89fd22400d74
JosephBerman <berman.jos@northeastern.edu>
2022-07-30 02:51:58 -0400

    end of night push

--- backend/api/natpark/NPSGrabber.py
+++ backend/api/natpark/NPSGrabber.py
@@ -7,9 +7,9 @@ PARKNUMBER = 467
 tokenfile = open("token.txt", 'r')
 token = tokenfile.readline().rstrip()
 
-def getAPICall(call: str, limit: int):
 
-    endpoint = 'https://developer.nps.gov/api/v1/' + call +'?limit=' + str(limit) +'&api_key=' + token
+def getAPICall(call: str, limit: int):
+    endpoint = 'https://developer.nps.gov/api/v1/' + call + '?limit=' + str(limit) + '&api_key=' + token
     return requests.get(endpoint)
 
 
@@ -22,10 +22,9 @@ parkList = []
 i = 0
 for park in parks['data']:
     parkList.append(
-        NatPark(i,park['fullName'], park['url'])
+        NatPark(i, park['fullName'], park['url'])
     )
-    i = i+1
+    i = i + 1
 
 for i in range(len(parkList)):
     parkList[i].printPark()
-
