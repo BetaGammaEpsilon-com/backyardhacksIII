ff38a020078bf8cdaeda1336a4d6d6ee798320bf
JosephBerman <berman.jos@northeastern.edu>
2022-07-29 22:28:41 -0400

    hello world demo works, followed tutorial

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,23 @@
 from flask import Flask
-
+import os
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'resources')
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
