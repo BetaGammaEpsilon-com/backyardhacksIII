19f60d587dca284ae5d3339dead92be4b0f9f7a0
JosephBerman <berman.jos@northeastern.edu>
2022-07-29 22:07:19 -0400

    Reconfigured files around

--- backend/api/db/db.py
+++ backend/api/db/db.py
@@ -5,6 +5,7 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 DATABASE = 'resources/database.db'
+SQLDATABASE = 'backend/api/db/sql/'
 
 
 def db_connect():
@@ -21,3 +22,26 @@ def db_close():
     conn = g.pop('conn', None)
     if conn is not None:
         conn.close()
+
+
+def db_init():
+    conn = db_connect()
+
+    file_path = SQLDATABASE + 'schema.sql'
+
+    with current_app.open_resource(file_path) as f:
+        conn.executescript(f.read().decode('utf8'))
+
+
+@click.command('db-init')
+@with_appcontext
+def db_init_command():
+    db_init()
+    click.echo("Initilized databse")
+
+
+def app_init(app):
+    app.teardown_appcontext(db_close())
+    app.cli.add_command(db_init_command)
+
+
